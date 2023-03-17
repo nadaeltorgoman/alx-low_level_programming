b3cb40a45d50baea5d290ceb0b5b484d83441ef0
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-16 17:29:26 -0700

    0x03. Shell, init files, variables and expansions

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints with write function
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.arr, 59);
+	return (1);
+}
