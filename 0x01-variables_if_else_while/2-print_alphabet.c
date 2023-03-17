885e66b24f22cea09db412886b2e28361360961a
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-17 16:44:40 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	for (char c = a; c <= z; c++;)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
