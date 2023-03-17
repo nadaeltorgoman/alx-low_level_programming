394e9fc6ddd721ee9ca425fe03461d1c45807d82
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-17 15:48:51 -0700

    0x03. Shell, init files, variables and expansions

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Description: A C program prints the last digit of variable n
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n % 10 == 0)
+	printf("Last digit of %d is 0 and is 0\n", n);
+	else if (n % 10 > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	return (0);
+}
