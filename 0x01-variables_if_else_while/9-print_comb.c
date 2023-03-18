041b85957d196c7ad38dc5f686d43da353a6df97
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-17 17:59:45 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints all single digit numbers of base 10.
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
