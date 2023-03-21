6ada70dae48279f552138dabdcf1ee85b535353f
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-21 16:59:44 -0700

    0x02 C - Functions, nested loops, with practice

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ **/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	putchar('\n');
+}
