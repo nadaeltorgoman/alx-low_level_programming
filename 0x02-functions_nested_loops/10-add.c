d798240c6154ecb5f9d4b930636ed2890a906a44
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-21 17:39:27 -0700

    0x02 C - Functions, nested loops, with practice

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * add - a function that adds two integers and returns the result
+ * @one: int to be added to two
+ * @two: int to be added to one
  * Return: one + two
  */
 int add(int one, int two)
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,7 @@
 #include "main.h"
 /**
  * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: print from this number
  * Return: Always 0 (Success)
  */
 void print_to_98(int n)
