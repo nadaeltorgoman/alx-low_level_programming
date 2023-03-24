a3d5d0e267025c654c4469563b43ec09c48a475b
nadaeltorgoman <nadoonada73@gmail.com>
2023-03-24 16:16:26 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,15 @@
 
 void print_line(int n)
 {
-	 if (n <= 0)
-		 _putchar('\n');
-	 else
-		 while (--n)
-		 {
-			 if (n <= 0)
-				 _putchar('\n');
-			 _putchar('_');
-		 }
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (n--)
+		{
+			if (n <= 0)
+				_putchar('\n');
+			_putchar('_');
+		}
+	}
 }
